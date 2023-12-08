3edca6d8dce5af42d8de346d0ac8cb8dcb4bfbae
Jeremy Qian <jq357@hotmail.com>
2023-12-07 21:33:56 -0500

    layout

--- src/components/refresh/refresh.js
+++ src/components/refresh/refresh.js
@@ -1,5 +1,22 @@
-export const refresh = (props) => {
+import { refreshToken } from "../../utils/index";
+import { useEffect, useState } from "react";
+
+export const Refresh = (props) => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    refreshToken()
+      .then(() => {})
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   const { children } = props;
+
+  if (loading) {
+    return null;
+  }
 
   return <>{children}</>;
 };
